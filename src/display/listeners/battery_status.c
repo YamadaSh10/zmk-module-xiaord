21a6381b31de08e3306a35e7834c6c19ffd2aa51
YamadaSh10 <yamada.sh10.358@gmail.com>
2026-03-23 23:05:59 +0900

    battery text color test

--- src/display/listeners/battery_status.c
+++ src/display/listeners/battery_status.c
@@ -54,20 +54,27 @@ static void periph_bat_update_cb(struct periph_bat_state state)
 	for (int i = 0; i < ZMK_SPLIT_CENTRAL_PERIPHERAL_COUNT; i++) {
 		lv_obj_t *arc = s_bat_arc[i];
 		lv_obj_t *lbl = s_bat_lbl[i];
+		lv_style_t style_label;
 
 		if (state.valid[i]) {
 			if (arc) {
 				lv_arc_set_value(arc, state.level[i]);
 			}
 			if (lbl) {
+				lv_style_copy(&style_label, &lv_style_plane);
+				style_label.text.color = lv_color_hex(0x76FF03); //light-green-accent-3
 				lv_label_set_text_fmt(lbl, "%d%%", state.level[i]);
+				lv_label_set_style(lbl, LV_LABEL_STYLE_MAIN, &style_label);
 			}
 		} else {
 			if (arc) {
 				lv_arc_set_value(arc, 0);
 			}
 			if (lbl) {
+				lv_style_copy(&style_label, &lv_style_plane);
+				style_label.text.color = lv_color_hex(0x76FF03); //light-green-accent-3
 				lv_label_set_text(lbl, "--");
+				lv_label_set_style(lbl, LV_LABEL_STYLE_MAIN, &style_label);
 			}
 		}
 	}
